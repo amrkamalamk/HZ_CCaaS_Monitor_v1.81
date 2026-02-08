ab55af3473cecfc102b0de005356a4a300cd129c
amrkamalamk <amrkamal.amk@gmail.com>
2026-02-08 16:39:41 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+/**
+ * Legacy App component minimized to resolve build conflicts.
+ * The main application logic now resides in app/page.tsx.
+ */
+const App: React.FC = () => {
+  return null;
+};
+
+export default App;
